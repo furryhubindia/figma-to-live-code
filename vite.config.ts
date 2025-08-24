bef357f56011414995dd18b28066c222e7be56a3
furryhubindia <furryhubindia@gmail.com>
2025-08-24 11:20:22 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/',
+  base: '/figma-to-live-code/',
   server: {
     host: "::",
     port: 8080,
